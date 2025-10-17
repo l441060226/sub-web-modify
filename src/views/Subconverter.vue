3786c3acc41e7936b819a1c9a1a3b037382380e3
l441060226 <101205045+l441060226@users.noreply.github.com>
2025-10-17 15:20:51 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,13 +339,13 @@ export default {
         },
         customBackend: {
           "自用专属后端": "https://subcomverter.xwz.us.kg",
-          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
-          "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
-          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
+         // "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
+         // "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
+         // "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
+        //  "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
+        //  nameless13提供: "https://www.nameless13.com",
+        //  subconverter作者提供: "https://sub.xeton.dev",
+       //   "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
           { value: "https://subapi.cmliussss.net" },
@@ -1311,4 +1311,5 @@ export default {
   }
 };
 </script>
+
 
